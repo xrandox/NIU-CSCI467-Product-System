a1e9809e96299d0fbbdf329550084d1ed9ffd2c1
xrandox <xrandox17@yahoo.com>
2023-03-21 23:19:01 -0500

    updated order model

--- src/backend/models/Order.js
+++ src/backend/models/Order.js
@@ -3,29 +3,30 @@
  */
 
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-
-/**
- * This schema represents a single ordered product within the order
- */
-const orderedProduct = new Schema({
-  productID: { type: mongoose.Types.ObjectId, required: true },
-  quantity: { type: Number, required: true },
-});
+const OrderedProduct = require("./OrderedProduct");
+const Address = require("./Address");
 
 /**
  * This schema represents a single order
  */
-const orderSchema = new Schema({
-  customerID: { type: mongoose.Types.ObjectId, required: true },
-  products: { type: [orderedProduct], required: true },
-  status: {
-    type: Number,
-    default: 0, //-1 - Cancelled, 0 - Processing, 1 - Shipped, 2 - Delivered
-    min: -1,
-    max: 2,
+const orderSchema = new Schema(
+  {
+    customer: { type: mongoose.Types.ObjectId, required: true },
+    shippingAddress: { type: Address.schema, required: true },
+    products: { type: [OrderedProduct.schema], required: true },
+    total: { type: mongoose.Types.Decimal128, required: true },
+    authorizationNumber: String,
+    authorizationTimestamp: Date,
+    status: {
+      type: Number,
+      default: 0, //-1 - Cancelled, 0 - Processing, 1 - Shipped, 2 - Delivered
+      min: -1,
+      max: 2,
+    },
+    shippedTimestamp: Date,
   },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Order", orderSchema);
