6e82110675026dea0a7de27dd7a19751e578a358
xrandox <xrandox17@yahoo.com>
2023-03-22 02:41:44 -0500

    comments/cleanup

--- src/backend/routes/profiles.js
+++ src/backend/routes/profiles.js
@@ -1,13 +1,14 @@
+/**
+ * This file contains the API routes for querying the profile system
+ */
 
 const express = require("express");
-var mongoose = require("mongoose");
-var User = mongoose.model("User");
 var auth = require("./auth");
-const { getProfile, prepareProfile } = require('../controllers/profiles');
+const { getProfile, prepareProfile } = require("../controllers/profiles");
 const router = express.Router();
 
 router.param("username", prepareProfile);
 
-router.get('/:username', auth.optional, getProfile);
+router.get("/:username", auth.optional, getProfile);
 
 module.exports = router;
