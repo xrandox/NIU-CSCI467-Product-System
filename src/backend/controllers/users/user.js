4526163da846be1425982e82bb8e6eb6c5a0bbde
xrandox <xrandox17@yahoo.com>
2023-03-24 01:18:28 -0500

    validation handling

---
+++ src/backend/controllers/users/user.js
@@ -0,0 +1,117 @@
+/**
+ * This file provides database functions for users that are then referenced by /routes/user.js
+ */
+
+const User = require("../../models/User");
+const passport = require("passport");
+
+/**
+ * Gets the current users authentication json by ID
+ */
+const getUser = async (req, res, next) => {
+  User.findById(req.auth.id)
+    .then(function (user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+
+      return res.json({ user: user.toAuthJSON() });
+    })
+    .catch(next);
+};
+
+/**
+ * Registers a new user in the database
+ * TODO: Add more registration fields?
+ * TODO: Also instead of letting mongoose do the validation, it would probably be better for the frontend
+ *       if we caught empty fields before sending them off to mongoose
+ */
+const registerUser = async (req, res) => {
+  try {
+    const user = new User();
+
+    user.username = req.body.user.username;
+    user.email = req.body.user.email;
+    user.setPassword(req.body.user.password);
+
+    const result = await user.save();
+    return res.json({ user: result.toAuthJSON() });
+  } catch (err) {
+    if (err.name === 'ValidationError') {
+      return res.status(400).json({ error: err.message });
+    }
+    res.status(500).json({ error: "Something went wrong" });
+  }
+};
+
+/**
+ * Checks provided login details against the saved credentials
+ */
+const loginUser = async (req, res, next) => {
+  if (!req.body.user.email) {
+    return res.status(422).json({ errors: { email: "can't be blank" } });
+  }
+
+  if (!req.body.user.password) {
+    return res.status(422).json({ errors: { password: "can't be blank" } });
+  }
+
+  passport.authenticate(
+    "local",
+    { session: false },
+    function (err, user, info) {
+      if (err) {
+        return next(err);
+      }
+
+      if (user) {
+        user.token = user.generateJWT();
+        return res.json({ user: user.toAuthJSON() });
+      } else {
+        return res.status(422).json(info);
+      }
+    }
+  )(req, res, next);
+};
+
+/**
+ * Updates a user
+ * TODO: Probably need to restrict what can be changed here, and allow admins a more powerful endpoint
+ */
+const updateUser = async (req, res, next) => {
+  User.findById(req.auth.id)
+    .then(function (user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+
+      // only update fields that were actually passed...
+      if (typeof req.body.user.username !== "undefined") {
+        user.username = req.body.user.username;
+      }
+      if (typeof req.body.user.email !== "undefined") {
+        user.email = req.body.user.email;
+      }
+      if (typeof req.body.user.password !== "undefined") {
+        user.setPassword(req.body.user.password);
+      }
+      if (typeof req.body.user.address !== "undefined") {
+        user.address = req.body.user.address;
+      }
+      if (typeof req.body.user.orders !== "undefined") {
+        user.orders = req.body.user.orders;
+      }
+
+      return user.save().then(function () {
+        return res.json({ user: user.toAuthJSON() });
+      });
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getUser,
+  registerUser,
+  loginUser,
+  updateUser,
+};
