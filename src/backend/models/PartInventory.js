fbf453b4df8c312ee7af62764a10bd229be243cf
xrandox <xrandox17@yahoo.com>
2023-03-30 02:59:07 -0500

    Update PartInventory.js
    - partnumber -> unique + indexed
    - quantity -> split into total, reserved and available
    - available is calculated on any create or save

--- src/backend/models/PartInventory.js
+++ src/backend/models/PartInventory.js
@@ -3,7 +3,7 @@
  */
 
 const mongoose = require("mongoose");
-
+const uniqueValidator = require("mongoose-unique-validator");
 const Schema = mongoose.Schema;
 
 /**
@@ -13,13 +13,41 @@ const partInventorySchema = new Schema({
   partNumber: {
     type: Number,
     required: true,
-    index: true
+    index: true,
+    unique: true,
   },
-  quantity: {
+  quantityAvailable: {
     type: Number,
     required: true,
     min: 0,
+    default: 0,
   },
+  quantityReserved: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 0,
+  },
+  quantityTotal: {
+    type: Number,
+    required: true,
+    min: 0,
+    default: 0,
+  },
+});
+
+partInventorySchema.plugin(uniqueValidator, {
+  message: "There is already inventory for this part",
+});
+
+partInventorySchema.pre("create", function (next) {
+  this.quantityAvailable = this.quantityTotal - this.quantityReserved;
+  next();
+});
+
+partInventorySchema.pre("save", function (next) {
+  this.quantityAvailable = this.quantityTotal - this.quantityReserved;
+  next();
 });
 
 module.exports = mongoose.model("PartInventory", partInventorySchema);
