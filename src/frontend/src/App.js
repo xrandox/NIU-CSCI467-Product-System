6e4e2815bda06c521f1bd2bacde7e23892e0e78e
xrandox <xrandox17@yahoo.com>
2023-05-05 14:36:04 -0500

    removed old page

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -7,7 +7,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 //Pages and Components
 import Home from "./pages/Home";
 import Store from "./pages/Store";
-import Inventory from "./pages/Inventory/Inventory";
 import Login from "./pages/Login";
 import AdminTools from "./pages/AdminTools";
 import Profile from "./pages/Profile";
@@ -23,12 +22,11 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/store/" element={<Store />} />
-            <Route path="/inventory/" element={<Inventory />} />
             <Route path="/login/" element={<Login />} />
             <Route path="/admin/" element={<AdminTools />} />
             <Route path="/profile/" element={<Profile />} />
