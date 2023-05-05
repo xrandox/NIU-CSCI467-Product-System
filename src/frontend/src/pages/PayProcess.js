3c6c05526c2c51c7863edf155eab9b3b04add0d3
xrandox <xrandox17@yahoo.com>
2023-05-05 16:25:56 -0500

    last changes

--- src/frontend/src/pages/PayProcess.js
+++ src/frontend/src/pages/PayProcess.js
@@ -2,70 +2,70 @@
 import React, { useState } from "react";
 import axios from "axios";
 import authorization from "../components/Auth";
-
+import { useNavigate, useParams } from "react-router-dom";
 
 export const PayProcess = () => {
+  const nav = useNavigate();
+  const { orderID } = useParams();
 
-  const [orderID, setorderID] = useState(null);
-  
   const [cc, setcc] = useState(null);
-  
+
   const [name, setname] = useState(null);
-  
+
   const [exp, setexp] = useState(null);
 
-    const submitPayment = () => {
-        axios.post("/api/cart/", 
-            {
-                "data": {
-                    "orderID": {orderID},
-                    "cc": cc,
-                    "name": name,
-                    "exp": exp
-                }
-            }
-        )
-        .then((response) => {
-            console.log(response.data)
-
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
-    return (
-        <div>
-            <h1>Payment Process</h1>
-            <input
-                id="orderID"
-                name="Order ID"
-                value={orderID}
-                onChange={(e) => setorderID(e.target.value)}
-            />
-            <input
-                id="cc"
-                name="Credit Card"
-                value={cc}
-                onChange={(e) => setcc(e.target.value)}
-            />
-            <input
-                id="name"
-                name="Name"
-                value={name}
-                onChange={(e) => setname(e.target.value)}
-            />
-            <input
-                id="exp"
-                name="Expiration Date"
-                value={exp}
-                onChange={(e) => setexp(e.target.value)}
-            />
-            <button onClick={() => {
-                submitPayment()
-                alert("Payment submitted!")
-            }}>Submit Payment</button>
-        </div>
-    );
-}
+  const submitPayment = () => {
+    axios
+      .post("/api/credit/", {
+        data: {
+          orderID: orderID,
+          cc: cc,
+          name: name,
+          exp: exp,
+        },
+      })
+      .then((response) => {
+        console.log(response.data);
+        nav("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  return (
+    <div>
+      <h1>Payment Process</h1>
+      <label htmlFor="cc">Credit Card Number</label>
+      <input
+        id="cc"
+        name="Credit Card"
+        value={cc}
+        onChange={(e) => setcc(e.target.value)}
+      />
+      <label htmlFor="name">Name on Card</label>
+      <input
+        id="name"
+        name="Name"
+        value={name}
+        onChange={(e) => setname(e.target.value)}
+      />
+      <label htmlFor="exp">Expiration</label>
+      <input
+        id="exp"
+        name="Expiration Date"
+        value={exp}
+        onChange={(e) => setexp(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          submitPayment();
+          alert("Payment submitted!");
+        }}
+      >
+        Submit Payment
+      </button>
+    </div>
+  );
+};
 
 export default authorization(PayProcess, ["user"]);
