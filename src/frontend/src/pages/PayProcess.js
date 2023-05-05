d52e12e5a27f29f79ea96de6a0923fab20e6e769
Michael Marchetti <mmarchetti515@gmail.com>
2023-05-05 16:12:28 -0500

    payment complete

--- src/frontend/src/pages/PayProcess.js
+++ src/frontend/src/pages/PayProcess.js
@@ -2,50 +2,69 @@
 import React, { useState } from "react";
 import axios from "axios";
 import authorization from "../components/Auth";
-import { useParams } from "react-router-dom";
+
 
 export const PayProcess = () => {
-  const { orderID } = useParams;
+
   const [orderID, setorderID] = useState(null);
+  
   const [cc, setcc] = useState(null);
+  
   const [name, setname] = useState(null);
+  
   const [exp, setexp] = useState(null);
 
-    const addToCart = () => {
+    const submitPayment = () => {
         axios.post("/api/cart/", 
             {
                 "data": {
-                    "orderID": "{{newOrderID}}",
-                    "cc": "6011 1234 4321 1234",
-                    "name": "John Doe",
-                    "exp": "12/2025"
+                    "orderID": {orderID},
+                    "cc": cc,
+                    "name": name,
+                    "exp": exp
                 }
             }
         )
         .then((response) => {
             console.log(response.data)
+            
         })
         .catch((error) => {
             console.log(error)
         })
     }
     return (
-        <div 
-            className="search-result"
-            // click on result to add to cart, display success message
-            onClick={() => {
-                addToCart()
-                alert("Part added to cart!")
-            }}
-        >
-                <input
-                    type="date"
-                    id="oldestDate"
-                    name="oldestDate"
-                    value={minDate}
-                    onChange={(e) => setMinDate(e.target.value)}
-                  />
-            { result.description }  ... Click to add to cart </div>
+        <div>
+            <h1>Payment Process</h1>
+            <input
+                id="orderID"
+                name="Order ID"
+                value={orderID}
+                onChange={(e) => setorderID(e.target.value)}
+            />
+            <input
+                id="cc"
+                name="Credit Card"
+                value={cc}
+                onChange={(e) => setcc(e.target.value)}
+            />
+            <input
+                id="name"
+                name="Name"
+                value={name}
+                onChange={(e) => setname(e.target.value)}
+            />
+            <input
+                id="exp"
+                name="Expiration Date"
+                value={exp}
+                onChange={(e) => setexp(e.target.value)}
+            />
+            <button onClick={() => {
+                submitPayment()
+                alert("Payment submitted!")
+            }}>Submit Payment</button>
+        </div>
     );
 }
 
